27a70d4cad2391f902ddc1c4abd162d6e86065ae
JoaoJorge <joaojorgedosanjos@gmail.com>
2022-10-17 16:35:02 -0300

    add js forEach

--- src/js/form.js
+++ src/js/form.js
@@ -1,47 +1,19 @@
-const inputBtn = document.getElementById("btn")
+const Btn = document.getElementById("btn")
+const entradas = document.querySelectorAll(".entrada")
+const mensagemError = document.querySelectorAll(".mensagem")
 
-
-inputBtn.addEventListener("click",(event)=>{
-    event.preventDefault()
-
-    const nome = document.getElementById("name")
-    const email = document.getElementById("email")
-    const telefone = document.getElementById("tel")
-    const mensagem = document.getElementById("mensagem")
-    const inputClass = document.getElementsByClassName("item")
-
-    
-    if(nome.value == ""){
-        inputClass[0].classList.remove("sucess")
-        inputClass[0].classList.add("error")
-    }else{
-        inputClass[0].classList.remove("error")
-        inputClass[0].classList.add("sucess")
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+  entradas.forEach((entradas, i) => {
+    console.log(mensagemError[i])
+    if (entradas.value == "") {
+      entradas.classList.remove("sucess");
+      entradas.classList.add("error");
+      mensagemError[i].classList.add("mensagem-error")
+    }else {
+      entradas.classList.remove("error");
+      entradas.classList.add("sucess");
+      mensagemError[i].classList.remove("mensagem-error")
     }
-    if(email.value == ""){
-        inputClass[1].classList.remove("sucess")
-        inputClass[1].classList.add("error")
-    }else if(email.value.indexOf("@")==-1 || email.value.indexOf(".")== -1){
-        inputClass[1].classList.remove("sucess")
-        inputClass[1].classList.add("error")
-    }
-    else{
-        inputClass[1].classList.remove("error")
-        inputClass[1].classList.add("sucess")
-    }
-    if(telefone.value == ""){
-        inputClass[2].classList.remove("sucess")
-        inputClass[2].classList.add("error")
-    }else{
-        inputClass[2].classList.remove("error")
-        inputClass[2].classList.add("sucess")
-    }
-    if(mensagem.value == ""){
-        inputClass[3].classList.remove("sucess")
-        inputClass[3].classList.add("error")
-    }else{
-        inputClass[3].classList.remove("error")
-        inputClass[3].classList.add("sucess")
-    }
-    
+  })
 })
